6eabe956d86e7eccd2abbec598fe43bf4d9a579b
CodeWater <1746559397@qq.com>
2022-04-07 23:53:20 +0800

    start springMVC

---
+++ SpringMVC/src/main/java/mvc/controller/HelloController.java
@@ -0,0 +1,27 @@
+package mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author ： CodeWater
+ * @create ：2022-04-07-22:38
+ * @Function Description ：
+ * 
+ */
+@Controller
+public class HelloController {
+
+    //  通过@RequestMapping注解，可以通过请求路径匹配要处理的具体的请求
+    //  /表示的当前工程的上下文路径 "/" -> /WEB-INF/templates/index.html
+    @RequestMapping("/")//省略了value
+    public String index(){
+//        返回的视图页面
+        return "index";
+    }
+    
+    @RequestMapping("/target")
+    public String toTarget(){
+        return "target";
+    }
+}
