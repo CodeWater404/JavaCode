e75d28164dca5947e09b626f1ed3c9ce34a8b1df
CodeWater <1746559397@qq.com>
2022-05-05 19:28:22 +0800

    add jvm

---
+++ Jvm/src/chapter01/StackStruTest.java
@@ -0,0 +1,16 @@
+package chapter01;
+
+/**
+ * @author ： CodeWater
+ * @create ：2022-05-05-17:46
+ * @Function Description ：
+ */
+public class StackStruTest {
+    public static void main(String[] args) {
+//        int i = 2 + 3;
+        int i = 2;
+        int j = 3;
+        int k = i + j ;
+        
+    }
+}
